a0800023befddb642be222b338d088cce1672608
Corey Donohoe <atmos@atmos.org>
2010-08-28 16:56:01 -0700

    add app code

---
+++ server.js
@@ -0,0 +1,9 @@
+// add the vendored express to the require path
+require.paths.unshift("vendor/ejs/lib")
+require.paths.unshift("vendor/connect/lib")
+require.paths.unshift("vendor/express/lib")
+require.paths.unshift("vendor/Socket.IO-node/lib")
+
+var app = require("./lib/app")
+
+app.run()
