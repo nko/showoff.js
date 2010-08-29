747cebd89b4da27bf57479ea617ba14b931641bf
Corey Donohoe <atmos@atmos.org>
2010-08-29 16:54:20 -0700

    simple markdown rendering of slides as a last commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // add the vendored express to the require path
 require.paths.unshift("vendor/ejs/lib")
+require.paths.unshift("vendor/showdown/lib")
 require.paths.unshift("vendor/connect/lib")
 require.paths.unshift("vendor/express/lib")
 require.paths.unshift("vendor/Socket.IO-node/lib")
